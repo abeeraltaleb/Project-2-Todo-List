a6d5d3c44aaf2722b0d97ccd2c882c27c2ae38c2
notme20n <abrn2009@hotmail.com>
2021-12-19 20:24:56 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,24 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/tasks",(req,res)=>{
-    res.json("get is working")
-})
-app.post("/test",(req,res)=>{
-    res.send("hello")
-})
+    Todo.find({},(err,data)=>{
+        if(err){
+            console.log("ERRO: ",err);
+        }else{
+            res.json(data)
+        }
+    });
+});
+app.post("/tasks",(req,res)=>{
+    console.log("25:", req.body)
+    Todo.create(req.body,(err,newTask)=>{
+        if(err){
+            console.log("ERRO: ",err);
+        }else{
+            res.status(201).json("Created New Todo Successfully");
+        }
+    });
+});
 
 app.listen(5000,()=>{
     console.log("server is working")
